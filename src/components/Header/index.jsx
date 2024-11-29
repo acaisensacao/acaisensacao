a51575823e3211c8ee248c98d13f718f35647392
acaisensacao <acaiisensacao@gmail.com>
2024-11-29 13:51:46 -0300

    committ

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -12,13 +12,13 @@ return (
     <div className='header'>
          <nav className="menu">
                     {/* <a onClick={showDrop} className='mobile-menu' > <IoMenu  /> </a> */}
-                    <a className='mobile-menu' > <IoMenu  /> </a>
+                    {/* <a className='mobile-menu' > <IoMenu  /> </a> */}
                     <div className="divlogo">
                          <Link to="/" className="logo"> <img src={logo}></img></Link>
                     </div>
                     <div className="div-list">
                         <ul>
-                        <li><Link className="item-menu" to="/">Início</Link></li>
+                        <li><Link className="item-menu" id='item-menu' to="/">Início</Link></li>
                         <li><Link className="item-menu" to="/cardapio">Cardápio</Link></li>
                         <li><Link className="item-menu" to="/contatos">Contatos/Localização</Link></li>
                         <a href="https://api.whatsapp.com/send?phone=5574988519565&text=Vim%20pelo%20Site!%20Me%20encaminha%20o%20card%C3%A1pio,%20por%20favor!"><button> Fazer Pedido</button></a>
