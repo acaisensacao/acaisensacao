0682aeb3ba74a493525849a6019996ae7044d234
acaisensacao <acaiisensacao@gmail.com>
2024-11-29 11:55:13 -0300

    teste2

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import { HashRouter, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home/index'
 import ContatosLocalizacao from './pages/ContatosLocalizacao/index'
 import Cardapio from './pages/Cardapio/index'
@@ -7,14 +7,14 @@ import Cardapio from './pages/Cardapio/index'
 
 export default function AppRoutes() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/cardapio' element={<Cardapio/>} />
         <Route path='/contatos' element={<ContatosLocalizacao/>} />
         <Route path='*' element={<Home/>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
    
   )
 }
